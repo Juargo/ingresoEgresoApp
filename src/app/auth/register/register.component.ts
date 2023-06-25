1b639acf4f04050b05def91d488f739989a4f542
Jorge R <jorge.retamal@zweicom.com>
2023-06-25 02:34:25 -0400

    feat: Se implemeneta loading en button crear usuario

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { AppState } from 'src/app/app.reducer';
 import { AuthService } from 'src/app/services/auth.service';
+import * as uiActions from 'src/app/shared/ui.actions';
+
 import Swal from 'sweetalert2';
 
 @Component({
@@ -9,13 +14,17 @@ import Swal from 'sweetalert2';
   templateUrl: './register.component.html',
   styles: [],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
+  subscription: Subscription = new Subscription();
+
   registroForm: FormGroup;
+  loading: boolean = false;
 
   constructor(
     private fb: FormBuilder,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private store: Store<AppState>
   ) {}
 
   ngOnInit(): void {
@@ -24,6 +33,9 @@ export class RegisterComponent implements OnInit {
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
     });
+    this.subscription.add(
+      this.store.select('ui').subscribe((ui) => (this.loading = ui.isLoading))
+    );
   }
 
   crearUsuario(): void {
@@ -31,17 +43,14 @@ export class RegisterComponent implements OnInit {
       return;
     }
 
+    this.store.dispatch(uiActions.isLoading());
+
     const { nombre, email, password } = this.registroForm.value;
 
     this.authService
       .createUser(nombre, email, password)
       .then((credenciales) => {
-        Swal.fire({
-          title: 'Espere por favor',
-          didOpen: () => {
-            Swal.showLoading();
-          },
-        });
+        this.store.dispatch(uiActions.stopLoading());
         this.router.navigate(['/']);
       })
       .catch((err) => {
@@ -50,6 +59,11 @@ export class RegisterComponent implements OnInit {
           icon: 'error',
           text: err.message,
         });
+        this.store.dispatch(uiActions.stopLoading());
       });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
